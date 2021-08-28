8ebb6a7b4af223c06cd9347d6b2711aa498ae65f
terrysu64 <66130506+terrysu64@users.noreply.github.com>
2021-08-28 17:14:14 -0400

    added files

--- src/index.js
+++
@@ -1,8 +0,0 @@
-//Date: August 13, 2021
-//Author: Terry Su
-//Purpose: A full-stack personal website to showcase my portfolio and some of my CS projects
-
-import initApp from "./js/index";
-import "./style/main.scss";
-
-initApp();
